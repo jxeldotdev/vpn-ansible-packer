05c3fcf025e85df78b1e2fdc191100d6086662da
Joel Freeman <joel@joelfreeman.xyz>
2020-10-31 10:17:15 +1300

    Updated playbooks & README.md

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-## Pritunl ansible
+## pritunl-ansible
 
+Ansible playbook(s) to install [Pritunl](https://pritunl.com/) on a CentOS 8 box.
 
-Set of ansible playbooks to setup Pritunl on CentOS 8 with automated MongoDB backups (Requires GCS bucket)
+Also configures a systemd service for automated mongodb backups.
+
+Playbook dependencies:
+- ansible.posix collection
--- templates/mongodb-backup.sh
+++ templates/mongodb-backup.sh
@@ -4,27 +4,27 @@ set -euo pipefail
 
 MONGO_URI="${1:-mongodb://localhost:27017/pritunl}"
 BACKUP_PATH="${2:-/var/mongo-backup/pritunl-$(date +%F)}"
-GCS_BUCKET="${3:-gcs://jf-personalcloud-prod-mongo-bkup}"
+GCS_BUCKET="${3:-gcs://jf-personalcloud-prod-mongo-bkup/}"
 
 # Check dependencies
 check_deps() {
-  local deps="gcloud mongodump"
+  local deps=(gsutil mongodump)
 
   for dep in $deps; do
-    if ! $dep; then echo "$dep is not installed, exiting" && exit 1; fi
+    if ! $dep -v; then echo "$dep is not installed, exiting" && exit 1; fi
   done
 
-  if ! test -d "$BACKUP_DIR"; then mkdir -p "$BACKUP_DIR"; fi
+  if ! test -d "$BACKUP_PATH"; then mkdir -p "$BACKUP_PATH"; fi
 }
 
 backup_db() {
   
   if mongodump --uri "$MONGO_URI" --gzip --out "$BACKUP_PATH"; then
-    gcloud cp "${BACKUP_PATH}" "${GCS_BUCKET}" && rm -rf "$BACKUP_PATH"
+    gsutil cp "${BACKUP_PATH}" "${GCS_BUCKET}" && rm -rf "$BACKUP_PATH"
   else
     # check if the backup is actually there, as it could be corrupted
     test -f "${BACKUP_PATH}" && rm -rf "${BACKUP_PATH}"
-    echo "Mongodb backup job to ${BACKUP_PATH} failed at $(date +%F-%H:%M)" 1>&2
+    echo "Mongodb backup job to ${BACKUP_PATH} failed at $(date +%F-%H:%M)" 1>&2 && exit 1
   fi
 }
 

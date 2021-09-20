57955159a50e906157ed7d821267858f4f30b78d
Joel Freeman <joel@jxel.dev>
2021-09-21 08:07:14 +1200

    Removed NAT Gateway

--- terraform/ci/vpc.tf
+++ terraform/ci/vpc.tf
@@ -9,11 +9,10 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.available_names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = []
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_vpn_gateway   = false
   enable_dns_hostnames = true
 }

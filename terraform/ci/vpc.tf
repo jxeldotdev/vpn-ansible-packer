78fc7b681cf1459f142faed75b57a26374291187
Joel Freeman <joel@jxel.dev>
2021-09-21 08:26:57 +1200

    Fixed incorrect data source

--- terraform/ci/vpc.tf
+++ terraform/ci/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   name = var.vpc_name
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availability_zones.available.available_names
+  azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 

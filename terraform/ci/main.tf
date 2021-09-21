296f5d79e768bba82524988226c6fc12f064821f
Joel Freeman <joel@jxel.dev>
2021-09-21 14:05:37 +1200

    Updated Role policy to have correct user arn

--- terraform/ci/main.tf
+++ terraform/ci/main.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
       type = "AWS"
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:${var.service_user_name}"
+        module.service_user.iam_user_arn
       ]
     }
   }
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
       type = "AWS"
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:${var.service_user_name}"
+        module.service_user.iam_user_arn   
       ]
     }
 
